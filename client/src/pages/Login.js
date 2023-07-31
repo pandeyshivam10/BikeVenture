9e2af096a2b6b23565bafe47bf16672d744ef482
Shivam Pandey <shivampandey12345678910@gmail.com>
2023-07-31 16:36:50 +0530

    minor change

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,13 +3,13 @@ import DefaultLayout from "../components/DefaultLayout";
 
 import { Form, Input, Button } from "antd";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 import { userLogin } from "../redux/actions/userActions";
 
 function Login() {
   const dispatch = useDispatch();
   const handleSubmit = (e) => {
-    dispatch(userLogin(e))
+    dispatch(userLogin(e));
   };
 
   useEffect(() => {
@@ -23,51 +23,53 @@ function Login() {
     <DefaultLayout>
       <div>
         <div className="login">
-           
-            
-              <Form
-                onFinish={handleSubmit}
-                className="login-form"
-                layout="vertical"
-              >
-                <h3 className="abc">Sign In</h3>
-                <hr />
-                <Form.Item
-                  name="username"
-                  rules={[
-                    { required: true, message: "Please input your username!" },
-                  ]}
-                >
-                  <Input prefix={<UserOutlined />} placeholder="Username" />
-                </Form.Item>
-                <Form.Item
-                  name="password"
-                  rules={[
-                    { required: true, message: "Please input your password!" },
-                  ]}
-                >
-                  <Input.Password
-                    prefix={<LockOutlined />}
-                    placeholder="Password"
-                  />
-                </Form.Item>
-                {/* <Form.Item>
+          <Form
+            onFinish={handleSubmit}
+            className="login-form"
+            layout="vertical"
+          >
+            <h3 className="abc">Sign In</h3>
+            <hr />
+            <Form.Item
+              name="username"
+              rules={[
+                { required: true, message: "Please input your username!" },
+              ]}
+            >
+              <Input prefix={<UserOutlined />} placeholder="Username" />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+            >
+              <Input.Password
+                prefix={<LockOutlined />}
+                placeholder="Password"
+              />
+            </Form.Item>
+            {/* <Form.Item>
                   <Form.Item name="remember" valuePropName="checked" noStyle>
                     <Checkbox>Remember me</Checkbox>
                   </Form.Item>
                   <a href="/forgot-password">Forgot password</a>
                 </Form.Item> */}
-                <Form.Item>
-                  <Button
-                    type="primary"
-                    htmlType="submit"
-                    className="login-form-button" style={{background:"black"}}
-                  >
-                    Log in
-                  </Button>
-                  <label>Or</label> <a style={{color: "yellow"}} href="/register">register now!</a>
-                </Form.Item>
-              </Form>
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+                style={{ background: "black" }}
+              >
+                Log in
+              </Button>
+              <label>Or</label>{" "}
+              <a style={{ color: "yellow" }} href="/register">
+                register now!
+              </a>
+            </Form.Item>
+          </Form>
         </div>
       </div>
     </DefaultLayout>
