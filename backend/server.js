1f11318e6a510a01781001aa0b253a1d45316c49
Shivam Pandey <shivampandey12345678910@gmail.com>
2024-03-04 23:11:02 +0530

    server cors

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,27 @@
 const express = require("express");
-require('dotenv').config();
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require('cors');
+const cors = require("cors");
 const dbConnection = require("./db");
-
 
 // console.log(process.env)
 
-
-app.use(express.json())
+app.use(express.json());
 
 app.use(cors());
 
-app.use("/api/bikes/", require("./routes/bikeRoutes"));
-app.use("/api/users", require("./routes/userRoutes"))
-app.use("/api/bookings", require("./routes/bookingRoutes"));
+app.use(
+  cors({
+    origin: "https://bike-venture.vercel.app",
+    methods: ["GET", "POST"], 
+    allowedHeaders: ["Content-Type", "Authorization"], 
+  })
+);
 
+app.use("/api/bikes/", require("./routes/bikeRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/bookings", require("./routes/bookingRoutes"));
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
