5d87f2eed4c51be40a1aeb9f71b7e1f6cd6dbdd6
Shivam Pandey <shivampandey12345678910@gmail.com>
2024-06-03 22:52:33 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -11,20 +11,24 @@ app.use(express.json());
 
 app.use(cors());
 
-
 app.use(
   cors({
-    origin: process.env.ORIGIN,
+    origin: process.env.ORIGIN, // ensure ORIGIN is set in your .env
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
+// Set additional security headers
+app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  next();
+});
 
 app.use("/api/bikes/", require("./routes/bikeRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/bookings", require("./routes/bookingRoutes"));
-
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
